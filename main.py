5952316651f0cc6f87c6f6bc950af9684be27424
Tom Penning <tom.penning@pm.me>
2025-02-25 13:00:41 +0100

    initial window created

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # Finanzplaner von Tom und Casper
+import sys
 
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
 
 
+layout = [[sg.Text("Test")],]
+
+
+def main():
+
+    window = sg.Window(title="Finanzplaner", layout=layout)
+
+    while True:
+        event, value = window.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
